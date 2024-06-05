1995cc3a8a8567929a71f396ad3af9ff60534338
jurgenstevens <jurgen.stevens@yahoo.com>
2024-06-04 19:40:30 -0500

    Include get request in games.http for show route.

--- GameStore/obj/Debug/net8.0/GameStore.AssemblyInfo.cs
+++ GameStore/obj/Debug/net8.0/GameStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+574dbed6f093cf457acc0bee6bfcea3ec34e36e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8abcde3ab906713f2dd8b69167bf4865ff341cde")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
