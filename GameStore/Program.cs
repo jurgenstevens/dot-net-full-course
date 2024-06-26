7b3e39188d603fcf19e118eaf516f41a5d09853b
jurgenstevens <jurgen.stevens@yahoo.com>
2024-06-25 23:59:33 -0500

    Stub up the builder for sqlite services with the connection string.

--- GameStore/Program.cs
+++ GameStore/Program.cs
@@ -1,8 +1,10 @@
+using GameStore.Data;
 using GameStore.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var connString = "Data Source=GameStore.db";
+builder.Services.AddSqlite<GameStoreContext>(connString);
 
 var app = builder.Build();
 
