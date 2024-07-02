d31092b2ab7da1e444cbf165951c2396d0c5b48c
jurgenstevens <jurgen.stevens@yahoo.com>
2024-07-02 15:00:27 -0500

    Stub up a mapping directory and a mapping file for the gamestore.

---
+++ GameStore/Mapping/GameMapping.cs
@@ -0,0 +1,6 @@
+﻿namespace GameStore.Mapping;
+
+public static class GameMapping
+{
+
+}
--- GameStore/obj/Debug/net8.0/GameStore.AssemblyInfo.cs
+++ GameStore/obj/Debug/net8.0/GameStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ba52178a8ffd3e1e96db2140a6557903d7d8ca8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da9a397f5e714be8b23d5e97f3c2ea0a259aa2d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
